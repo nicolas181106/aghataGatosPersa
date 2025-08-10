6534d1b68a64dfbba40dfd5865e39dd26984de34
nicolas181106 <nicolasbernalbermudez@gmail.com>
2025-08-10 13:51:53 +0200

    Muchas mejoras

---
+++ src/scripts/gatos-europeo.js
@@ -0,0 +1,25 @@
+import img4 from "../assets/img/romulo.jpg";
+import img5 from "../assets/img/remo.jpg";
+
+export const gatosEuropeo = [
+	{
+		imgSrc: img4,
+		alt: "Gatito Rómulo",
+		nombre: "Rómulo",
+		genero: "Masculino",
+		edad: new Date("2025-05-26"),
+		slugEuropeo: "romulo",
+		raza: "europeo",
+		texto: "Rómulo es un gato persa europeo de pelaje suave y de color marrón con grises. Es tranquilo, dormilón y cariñoso. Con un ronroneo que parece activarse cuando pones tu mano sobre él.",
+	},
+	{
+		imgSrc: img5,
+		alt: "Gatito Remo",
+		nombre: "Remo",
+		genero: "Masculino",
+		edad: new Date("2025-05-26"),
+		slugEuropeo: "remo",
+		raza: "europeo",
+		texto: "Remo es un gato persa europeo de pelaje suave y de color gris, marrón y tonalidades blancas como su padre. Es muy juguetón y curioso, pero también sabe tomarse sus descansos cuando es necesario.",
+	}
+];
