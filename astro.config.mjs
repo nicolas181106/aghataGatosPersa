0eab2eb09e93da552d7bb10050ccdedf9b8636f8
nicolas181106 <nicolasbernalbermudez@gmail.com>
2026-03-10 16:37:20 +0100

    Actualizacion Marzo 2026 / 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
 import tailwindcss from "@tailwindcss/vite";
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
+    output: 'server',
+    adapter: netlify(), 
     vite: {
-    plugins: [tailwindcss()],
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve('./src')
+            }
+        }
     },
 });
