4daa7170121bcf9be5cfc17ff0d4a122a7fd1f83
nicolas.bernal <nicolas.bernal@ext.experienceis.es>
2026-03-12 19:33:21 +0100

    Actualizacion de interfaz

--- src/scripts/gatos-europeo.js
+++ src/scripts/gatos-europeo.js
@@ -1,30 +1,4 @@
 import unknown from "../assets/img/unknown.jpg";
 
 export const gatosEuropeo = [
-    /*{
-        name: "Esmeralda",
-        gender: "Femenino",
-        birth: "2025-12-02",
-        img: esmeraldaBebe.src,
-        img2: esmeraldaBebe2.src,
-        alt: "esmeraldaimg",
-        slugEuropeo: "esmeralda",
-        text: "Gato con pelaje suave y ojos brillantes",
-        raza: "europeo",
-        disponibleEstilo: "absolute top-4 right-4 bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
-        disponibleTexto: "✗ Adoptado",
-    },
-    {
-        name: "Heracles",
-        gender: "Macho",
-        birth: "2025-12-02",
-        img: heraclesBebe.src,
-        img2: heraclesBebe2.src,
-        alt: "heraclesimg",
-        slugEuropeo: "heracles",
-        text: "Gato con pelaje suave y ojos brillantes",
-        raza: "europeo",
-        disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
-        disponibleTexto: "✓ Disponible",
-    },*/
 ];
--- src/scripts/modelo.js
+++ src/scripts/modelo.js
@@ -7,7 +7,12 @@ export const gatosEuropeo = [
         img2: unknown.src,
         alt: "",
         slugEuropeo: "",
+        /*slugChinchilla o slugHimalayo*/
         text: "",
         raza: "",
+        disponibleEstilo: "absolute top-4 right-4 bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
+        disponibleTexto: "✗ Adoptado",
+        /*disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
+        disponibleTexto: "✓ Disponible",*/
     }
 ]
