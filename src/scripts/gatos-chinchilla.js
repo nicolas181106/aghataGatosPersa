00c78d61923bedab1d5942e5414a02581e9ae3da
nicolas.bernal <nicolas.bernal@ext.experienceis.es>
2026-08-26 13:36:00 +0200

    Actualizacion

--- src/scripts/gatos-chinchilla.js
+++ src/scripts/gatos-chinchilla.js
@@ -3,8 +3,8 @@ import polar1 from "../assets/img/nuestros_gatos/Ceres/Camada4/Polar/Polar1.jpeg
 import polar2 from "../assets/img/nuestros_gatos/Ceres/Camada4/Polar/Polar2.jpeg"
 import artico1 from "../assets/img/nuestros_gatos/Ceres/Camada4/Artico/Artico1.jpeg"
 import artico2 from "../assets/img/nuestros_gatos/Ceres/Camada4/Artico/Artico2.jpeg"
-import zorro1 from "../assets/img/nuestros_gatos/Ceres/Camada4/Zorro/zorro1.jpeg"
-import zorro2 from "../assets/img/nuestros_gatos/Ceres/Camada4/Zorro/zorro2.jpeg"
+import zorro1 from "../assets/img/nuestros_gatos/Ceres/Camada4/Zorro/Zorro1.jpeg"
+import zorro2 from "../assets/img/nuestros_gatos/Ceres/Camada4/Zorro/Zorro2.jpeg"
 import mercurio1 from "../assets/img/nuestros_gatos/Ceres/Camada4/Mercurio/Mercurio1.jpeg"
 import mercurio2 from "../assets/img/nuestros_gatos/Ceres/Camada4/Mercurio/Mercurio2.jpeg"
 
@@ -50,7 +50,7 @@ export const gatosChinchilla = [
         img2: mercurio2.src,
         alt: "Camada de gatos persas chinchilla",
         color: "Gris",
-        disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
-        disponibleTexto: "✓ Disponible",
+        disponibleEstilo: "absolute top-4 right-4 bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
+        disponibleTexto: "✗ Adoptado",
     }
 ];
