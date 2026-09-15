ac3b9f72418b25365e5cac4d7fd338dd1ebc681d
nicolas.bernal <nicolas.bernal@ext.experienceis.es>
2026-09-15 21:44:49 +0200

    Nuevas fotos de gatos

--- src/scripts/gatos-europeo.js
+++ src/scripts/gatos-europeo.js
@@ -1,49 +1,57 @@
 import unknown from "../assets/img/gatos/unknown.jpg";
-import tigre from "../assets/img/nuestros_gatos/Bluey/Camada4/Tigre/Tigre435.jpeg"
-import tigre2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Tigre/Tigre436.jpeg"
+import yeti from "../assets/img/nuestros_gatos/Bluey/Camada4/Yeti/yeti1.jpeg"
+import yeti2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Yeti/yeti2.jpeg"
+import perla from "../assets/img/nuestros_gatos/Bluey/Camada4/Perla/perla1.jpeg"
+import perla2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Perla/perla2.jpeg"
+import coco from "../assets/img/nuestros_gatos/Bluey/Camada4/Coco/coco2.jpeg"
+import coco2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Coco/coco1.jpeg"
+import shiro from "../assets/img/nuestros_gatos/Bluey/Camada4/Shiro/shiro1.jpeg"
+import shiro2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Shiro/shiro2.jpeg"
 import pantera from "../assets/img/nuestros_gatos/Bluey/Camada4/Pantera/Pantera12.jpeg"
 import pantera2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Pantera/Pantera123.jpeg"
+import tigre from "../assets/img/nuestros_gatos/Bluey/Camada4/Tigre/Tigre435.jpeg"
+import tigre2 from "../assets/img/nuestros_gatos/Bluey/Camada4/Tigre/Tigre436.jpeg"
 
 export const gatosEuropeo = [
     {
-        name: "",
-        gender: "",
+        name: "Yeti",
+        gender: "Macho",
         birth: "2026-08-24",
-        img: unknown.src,
-        img2: unknown.src,
+        img: yeti.src,
+        img2: yeti2.src,
         alt: "Camada de gatos persas europeos",
         color: "Blanco",
         disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
         disponibleTexto: "✓ Disponible",
     },
     {
-        name: "",
-        gender: "",
+        name: "Perla",
+        gender: "Hembra",
         birth: "2026-08-24",
-        img: unknown.src,
-        img2: unknown.src,
+        img: perla.src,
+        img2: perla2.src,
         alt: "Camada de gatos persas europeos",
         color: "Blanco",
         disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
         disponibleTexto: "✓ Disponible",
     },
     {
-        name: "",
-        gender: "",
+        name: "Coco",
+        gender: "Hembra",
         birth: "2026-08-24",
-        img: unknown.src,
-        img2: unknown.src,
+        img: coco.src,
+        img2: coco2.src,
         alt: "Camada de gatos persas europeos",
         color: "Blanco",
         disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
         disponibleTexto: "✓ Disponible",
     },
     {
-        name: "",
-        gender: "",
+        name: "Shiro",
+        gender: "Hembra",
         birth: "2026-08-24",
-        img: unknown.src,
-        img2: unknown.src,
+        img: shiro.src,
+        img2: shiro2.src,
         alt: "Camada de gatos persas europeos",
         color: "Blanco",
         disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
@@ -68,7 +76,7 @@ export const gatosEuropeo = [
         img2: tigre2.src,
         alt: "Camada de gatos persas europeos",
         color: "Atigrado",
-        disponibleEstilo: "absolute top-4 right-4 bg-green-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
-        disponibleTexto: "✓ Disponible",
+        disponibleEstilo: "absolute top-4 right-4 bg-red-500 text-white text-xs font-bold px-3 py-1 rounded-full shadow-lg",
+        disponibleTexto: "✗ Adoptado",
     },
 ];
